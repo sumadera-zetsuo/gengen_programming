5084511d6e1ccdcf91df0bf0e92fe7f6b99215fe
sumadera_zetsuo <86052459+sumadera-zetsuo@users.noreply.github.com>
2021-06-23 11:51:04 +0900

    Update Sample2.java

--- src/gengen_programming/Sample2.java
+++ src/gengen_programming/Sample2.java
@@ -1,1 +1,12 @@
+// パッケージ名
+package gengen_programming;
+
+// クラス名(=ファイル名と同じ名前にして、先頭は大文字にする)
+public class Sample2 {
+	// メインメソッドを定義する。
+	public static void main(String[] args) {
+		// コンソールに文字列を出力させる。
+		System.out.println("HelloWorld!!");
+	}
+}
 
