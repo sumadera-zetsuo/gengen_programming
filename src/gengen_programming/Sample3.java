2c70fe9d6416faf735c2c6700e9bc5faf8ba98d6
sumadera_zetsuo <sumadera.zetsuo@gmail.com>
2021-06-23 13:24:03 +0900

    Sample3を新規作成

---
+++ src/gengen_programming/Sample3.java
@@ -0,0 +1,28 @@
+// パッケージ名
+package gengen_programming;
+
+// クラス名(=ファイル名と同じ名前にして、先頭は大文字にする)
+public class Sample3 {
+	// メインメソッドを定義する。
+	public static void main(String[] args) {
+		// 変数と型を定義する。
+		String name = "田中太郎";
+		int age = 20;
+		double height = 170.6;
+
+		// コンソールに変数の内容を出力する。
+		System.out.println("私の名前は" + name + "です。");
+		System.out.print("年齢は" + age + "歳で、");
+		System.out.println("身長は" + height + "cmです。");
+
+		// 変数の値を変更する
+		name = "田中次郎";
+		age = 18;
+		height = 173.8;
+
+		// コンソールに変数の内容を出力する。
+		System.out.println("私の名前は" + name + "です。");
+		System.out.print("年齢は" + age + "歳で、");
+		System.out.println("身長は" + height + "cmです。");
+	}
+}
