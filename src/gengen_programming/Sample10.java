6205e180f98e43090bf21b2b30a6068255662a7c
gengen <gengen@gengen-kun>
2021-06-28 22:53:25 +0900

    田中正蔵の年齢を変更しました

--- src/gengen_programming/Sample10.java
+++ src/gengen_programming/Sample10.java
@@ -5,7 +5,7 @@ public class Sample10 {
 	public static void main(String[] args) {
 		// 変数と型を定義する。
 		String name = "田中正造";
-		int age = 56;
+		int age = 52;
 		double height = 165.6;
 		Sample9.introduce(name, age, height);
 	}
