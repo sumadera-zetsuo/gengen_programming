cf59264bb558dc709d19cb608ef81bb5567cb48a
gengenpso <62192618+gengenpso@users.noreply.github.com>
2021-07-04 22:47:57 +0900

    Sample10を修正しました

--- src/gengen_programming/Sample10.java
+++ src/gengen_programming/Sample10.java
@@ -5,9 +5,8 @@ public class Sample10 {
 	public static void main(String[] args) {
 		// 変数と型を定義する。
 		String name = "田中正造";
-		int age = 52;
-		double height = 165.6;
+		int age = 56;
+		double height = 165.4;
 		Sample9.introduce(name, age, height);
 	}
-
 }
