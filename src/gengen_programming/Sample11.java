e40cded41e2d1fd3615ebacdf37a559fbe681ff1
shun <t10092ss@gmail.com>
2021-07-04 21:51:44 +0900

    Sample11を修正

--- src/gengen_programming/Sample11.java
+++ src/gengen_programming/Sample11.java
@@ -6,12 +6,20 @@ public class Sample11 {
 		// 変数と型を定義する。
 		int num1 = 4;
 		int num2 = 7;
-		int sum = add(num1, num2);
-		System.out.println("合計は" + sum + "です。");
+		int sum1 = add(num1, num2);
+		System.out.println("合計は" + sum1 + "です。");
+		int dif = min(num1, num2);
+		System.out.println("合計は" + dif + "です。");
 	}
 
-	public static int add(int num1, int num2) {
-		return num1 + num2;
+	// 数値を2つ受け取るとその値を足した値が返ってくる 
+	public static int add(int num1, int num2) { // int num1,num2引数
+		return num1 + num2; // 戻り値
+	}
+
+	// 数値を2つ受け取るとその値を引いた値が返ってくる 
+	public static int min(int num1, int num2) { // int num1,num2引数
+		return num1 - num2; // 戻り値
 	}
 
 }
