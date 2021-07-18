ea1406641f9d84b3142be5f29ba9a3c0d5b289c4
sumadera_zetsuo <86052459+sumadera-zetsuo@users.noreply.github.com>
2021-07-18 16:18:01 +0900

    Update Cart.java

--- src/gengen_programming/Cart.java
+++ src/gengen_programming/Cart.java
@@ -1,40 +1,43 @@
 package gengen_programming;
 
 public class Cart {
-	String name;
-	int price;
-	int speed;
-
+	//フィールド
+	public String name;
+	public int price;
+	public int speed;
+	
+	//アクセサー
+	//ゲッター
 	public String getName() {
 		return name;
 	}
-
+	//セッター
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	//ゲッター
 	public int getPrice() {
 		return price;
 	}
-
+	//セッター
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
+	//ゲッター
 	public int getSpeed() {
 		return speed;
 	}
-
+	//セッター
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
-
-	public void speedUp() {
+	//メソッド
+	public void () {
 		speed += 10;
 	}
-
-	public void speedDown() {
+	//メソッド
+	public void break_() {
 		speed -= 10;
 	}
-
+	
 }
